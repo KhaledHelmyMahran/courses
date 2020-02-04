8cd5f183f844603d0cf67d256cb47e4bfcaf70f8
KhaledHelmyMahran <khaledhelmy774@gmail.com>
2020-02-04 10:08:03 +0200

    Updates

---
+++ precache-manifest.346f6a75076bc25f681df1f6eba9f34d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c254fd4d68849147edf632681c7a8020",
+    "url": "/courses/index.html"
+  },
+  {
+    "revision": "4d4b588fbdc8438560c9",
+    "url": "/courses/static/css/main.ea511fda.chunk.css"
+  },
+  {
+    "revision": "9f5a4c0ef8074e713d62",
+    "url": "/courses/static/js/2.11a16097.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/courses/static/js/2.11a16097.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4d4b588fbdc8438560c9",
+    "url": "/courses/static/js/main.4ab86893.chunk.js"
+  },
+  {
+    "revision": "b38e945a59983dd0c6dd",
+    "url": "/courses/static/js/runtime-main.6172fb4c.js"
+  }
+]);
